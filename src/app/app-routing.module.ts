e6bd71c0a94675eda83b533bccebf593b1433d79
Simon <simon.bao@student.vinci.be>
2020-12-02 18:50:37 +0100

    conflict resolved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {CompanyRegisterFormComponent} from './company-register-form/company-register-form.component';
-import { RegisterCompanyFormComponent } from 'app/register-company-form/register-company-form.component';
+import {CompanyRegisterFormComponent} from 'app/company-register-form/company-register-form.component';
+
 
 const routes: Routes = [
   { path: 'register', component: CompanyRegisterFormComponent }
 ];
-  { path: 'addCompany', component: RegisterCompanyFormComponent }
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CompanyRegisterFormComponent } from './company-register-form/company-register-form.component';
-import { RegisterCompanyFormComponent } from './register-company-form/register-company-form.component';
+import { AppRoutingModule } from 'app/app-routing.module';
+import { AppComponent } from 'app/app.component';
+import { CompanyRegisterFormComponent } from 'app/company-register-form/company-register-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CompanyRegisterFormComponent
-    AppComponent,
-    RegisterCompanyFormComponent
   ],
   imports: [
     BrowserModule,
