2fcdcb83fc80ce8f2f86bbb370d684b0c35a1f45
Alessio <alessio.soldi@student.vinci.be>
2020-12-03 21:19:12 +0100

    Impl Generate QRCode not complete

--- src/app/company-qrcode-form/company-qrcode-form.component.ts
+++ src/app/company-qrcode-form/company-qrcode-form.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CompanyService} from '../../services/company.service';
+import {Company} from '../../model/company';
+import {Qrcode} from '../../model/qrcode';
 
 @Component({
   selector: 'app-company-qrcode-form',
   templateUrl: './company-qrcode-form.component.html',
   styleUrls: ['./company-qrcode-form.component.sass']
 })
-export class CompanyQrcodeFormComponent implements OnInit {
+export class CompanyQrcodeFormComponent {
 
-  constructor() { }
+  qrcode: Qrcode;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private companyQrcodeGeneratorService: CompanyService
+  ) {
+    this.qrcode = new Qrcode();
+  }
+
+  onSubmit() {
+    this.companyQrcodeGeneratorService.;
   }
 
 }
